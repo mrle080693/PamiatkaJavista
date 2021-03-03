6e212a2774c18afeb9968019064aed22fbc346c5
Лебедев Александр Сергеевич <mrle080693@gmail.com>
2021-03-03 15:43:14 +0200

    Creating of MVP done. Now I need content and deploy.

--- src/main/java/com/prostosasha/pamiatkajavista/controllers/CommentsController.java
+++ src/main/java/com/prostosasha/pamiatkajavista/controllers/CommentsController.java
@@ -4,47 +4,25 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.prostosasha.pamiatkajavista.models.Comment;
 import com.prostosasha.pamiatkajavista.services.CommentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+@Controller
 @RequestMapping("/comments")
 public class CommentsController {
     @Autowired
     private CommentService commentService;
-    @Autowired
-    ObjectMapper objectMapper;
 
     @RequestMapping(method = RequestMethod.POST)
-    public Comment save(@RequestParam String commentJson) {
-        Comment savedComment = new Comment();
-
-        System.out.println(commentJson);               // Костыль !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    public String save(@RequestParam String pageName, @RequestParam String userName, @RequestParam String comment) {
         try {
-            Comment comment = objectMapper.readValue(commentJson, Comment.class);
-            savedComment = commentService.save(comment);
-            System.out.println(savedComment.toString());               // Костыль !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            Comment objComment = new Comment(pageName, userName, comment);
+            commentService.save(objComment);
         } catch (Exception e) {
             // LOGGER !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
             e.printStackTrace();
         }
 
-        return savedComment;
+        return "redirect:/" + pageName;
     }
-
-    /*
-    Just Recomment it and server ready to work with mobile apps
-    @RequestMapping(method = RequestMethod.GET)
-    public List<Comment> findAllByPageName(@PathVariable String pageName){
-        List<Comment> comments = new ArrayList<>();
-
-        try {
-            comments = commentService.findAllByPageName(pageId);
-        } catch (Exception e) {
-            // LOGGER !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-            e.printStackTrace();
-        }
-
-        return comments;
-    }
-    */
 }
--- src/main/java/com/prostosasha/pamiatkajavista/controllers/PagesController.java
+++ src/main/java/com/prostosasha/pamiatkajavista/controllers/PagesController.java
@@ -3,18 +3,11 @@ package com.prostosasha.pamiatkajavista.controllers;
 import com.prostosasha.pamiatkajavista.models.Comment;
 import com.prostosasha.pamiatkajavista.services.CommentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.InputStreamResource;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.util.List;
 
 @Controller
